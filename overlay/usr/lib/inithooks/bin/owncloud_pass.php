2187ff26e84d6ee4cd9208cac581e05d66602da5
Alon Swartz <alon@turnkeylinux.org>
2013-03-11 13:44:13 +0200

    updated inithooks for new version

--- overlay/usr/lib/inithooks/bin/owncloud_pass.php
+++ overlay/usr/lib/inithooks/bin/owncloud_pass.php
@@ -1,13 +1,15 @@
 <?php
 
+include '/var/www/owncloud/config/config.php';
 include '/var/www/owncloud/3rdparty/phpass/PasswordHash.php';
 
 if(count($argv)!=2) die("usage: $argv[0] password\n");
 
 $password = $argv[1];
+$salt = $CONFIG['passwordsalt'];
 
 $PasswordHash = new PasswordHash(8, false);
-print $PasswordHash->HashPassword($password);
+print $PasswordHash->HashPassword($password.$salt);
 
 ?>
 
